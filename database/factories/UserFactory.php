b7495a326fb6f121ccab294af19b67d7d7d188fe
nvlight <nvlight@mail.ru>
2020-05-07 22:21:14 +0300

    refactoring. Add status features for user, add mailVerify

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -15,12 +15,16 @@ use Faker\Generator as Faker;
 |
 */
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$f8DTh5RLBwq.lkAcWuphzOdE2CHP1E.2/hv1sthhTZ2Z57zOQR7ea', // 111111
         'remember_token' => Str::random(10),
+        'status' => User::STATUS_ACTIVE,
     ];
 });
