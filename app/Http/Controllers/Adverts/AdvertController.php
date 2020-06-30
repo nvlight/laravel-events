c527f813a3fe2db0f0ebae116d686ab1bb6221c6
nvlight <nvlight@mail.ru>
2020-06-30 14:47:21 +0300

    add searcg aggregation

--- app/Http/Controllers/Adverts/AdvertController.php
+++ app/Http/Controllers/Adverts/AdvertController.php
@@ -14,6 +14,7 @@ use App\Http\Requests\Adverts\SearchRequest;
 use App\UseCases\Adverts\SearchService;
 //use App\ReadModel\AdvertReadRepository; ?
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Pagination\Paginator;
 
 class AdvertController extends Controller
 {
@@ -40,18 +41,44 @@ class AdvertController extends Controller
 //            $query->forRegion($region);
 //        }
 
-        $regions = $region
-            ? $region->children()->orderBy('name')->getModels()
-            : Region::roots()->orderBy('name')->getModels();
+//        $regions = $region
+//            ? $region->children()->orderBy('name')->getModels()
+//            : Region::roots()->orderBy('name')->getModels();
+//
+//        $categories = $category
+//            ? $category->children()->defaultOrder()->getModels()
+//            : Category::whereIsRoot()->defaultOrder()->getModels();
 
-        $categories = $category
-            ? $category->children()->defaultOrder()->getModels()
-            : Category::whereIsRoot()->defaultOrder()->getModels();
+        $result = $this->search->search($category, $region, $request, 20, $request->get('page', 1));
+        $adverts = $result->adverts;
+        $regionsCounts = $result->regionsCounts;
+        $categoriesCounts = $result->categoriesCounts;
 
         //$adverts = $query->paginate(20);
-        $adverts = $this->search->search($category, $region, $request, 20, $request->get('page', 1));
+        //$adverts = $this->search->search($category, $region, $request, 20, $request->get('page', 1));
 
-        return view('adverts.index', compact('category', 'region', 'categories', 'regions', 'adverts'));
+        $query = $region ? $region->children() : Region::roots();
+        $regions = $query->orderBy('name')->getModels();
+
+        //return view('adverts.index', compact('category', 'region', 'categories', 'regions', 'adverts'));
+
+        $query = $category ? $category->children() : Category::whereIsRoot();
+        $categories = $query->defaultOrder()->getModels();
+
+        $regions = array_filter($regions, function (Region $region) use ($regionsCounts) {
+            return isset($regionsCounts[$region->id]) && $regionsCounts[$region->id] > 0;
+        });
+
+        $categories = array_filter($categories, function (Category $category) use ($categoriesCounts) {
+            return isset($categoriesCounts[$category->id]) && $categoriesCounts[$category->id] > 0;
+        });
+
+        return view('adverts.index', compact(
+            'category', 'region',
+            'categories', 'regions',
+            'regionsCounts', 'categoriesCounts',
+            'adverts'
+        ));
     }
 
     public function show(Advert $advert)
--- resources/views/adverts/index.blade.php
+++ resources/views/adverts/index.blade.php
@@ -19,7 +19,11 @@
                                 @foreach ($chunk as $current)
 {{--                                    <li><a href="{{ route('adverts.index', [$region, $current]) }}">{{ $current->name }}</a></li>--}}
 {{--                                    <li><a href="{{ route('adverts.index', adverts_path($region, $current)) }}">{{ $current->name }}</a></li>--}}
-                                    <li><a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($region, $current)], request()->all())) }}">{{ $current->name }}</a></li>
+{{--                                    <li><a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($region, $current)], request()->all())) }}">{{ $current->name }}</a></li>--}}
+                                    <li>
+                                        <a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($region, $current)], request()->all())) }}">{{ $current->name }}</a>
+                                        ({{ $categoriesCounts[$current->id] ?? 0 }})
+                                    </li>
                                 @endforeach
                             </ul>
                         </div>
@@ -46,7 +50,11 @@
                                 @foreach ($chunk as $current)
 {{--                                    <li><a href="{{ route('adverts.index', [$current, $category]) }}">{{ $current->name }}</a></li>--}}
 {{--                                    <li><a href="{{ route('adverts.index', adverts_path($current, $category)) }}">{{ $current->name }}</a></li>--}}
-                                    <li><a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($current, $category)], request()->all())) }}">{{ $current->name }}</a></li>
+{{--                                    <li><a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($current, $category)], request()->all())) }}">{{ $current->name }}</a></li>--}}
+                                    <li>
+                                        <a href="{{ route('adverts.index', array_merge(['adverts_path' => adverts_path($current, $category)], request()->all())) }}">{{ $current->name }}</a>
+                                        ({{ $regionsCounts[$current->id] ?? 0 }})
+                                    </li>
                                 @endforeach
                             </ul>
                         </div>
