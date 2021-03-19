eac275febf3457d0c96be602a8783d178bbf88df
nvlight <lightstyle11@mail.ru>
2021-03-19 20:32:33 +0300

    anather part for tagvalue gistogramm

--- resources/views/test/js_diagramms/gistogramm.blade.php
+++ resources/views/test/js_diagramms/gistogramm.blade.php
@@ -12,7 +12,7 @@
     <legend for="js_gistogramm_month"></legend>
     <script>
         let canvas = document.getElementById("js_gistogramm_month");
-        canvas.width = 300;
+        canvas.width = 900;
         canvas.height = 300;
 
         let ctx = canvas.getContext("2d");
@@ -41,10 +41,17 @@
             this.colors = options.colors;
 
             this.draw = function(){
-                var maxValue = 0;
-                for (var categ in this.options.data){
-                    maxValue = Math.max(maxValue,this.options.data[categ]);
+
+                let numberOfBars = 0;
+                let maxValue = 0;
+                for (let i in this.options.data) {
+                    for (let j in this.options.data[i]) {
+                        maxValue = Math.max(maxValue, this.options.data[i][j]);
+                        numberOfBars++;
+                    }
                 }
+                //console.log('maxValue: '+maxValue);
+
                 var canvasActualHeight = this.canvas.height - this.options.padding * 2;
                 var canvasActualWidth = this.canvas.width - this.options.padding * 2;
 
@@ -72,69 +79,114 @@
                 // }
 
                 //drawing the bars
-                var barIndex = 0;
-                var numberOfBars = Object.keys(this.options.data).length;
-                var barSize = (canvasActualWidth)/numberOfBars;
+                let barIndex = 0;
+                let barSize = (canvasActualWidth)/numberOfBars;
 
+                let offset = 0; //for
                 for (categ in this.options.data){
-                    var val = this.options.data[categ];
-                    var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
-                    drawBar(
-                        this.ctx,
-                        this.options.padding + barIndex * barSize,
-                        this.canvas.height - barHeight - this.options.padding,
-                        barSize,
-                        barHeight,
-                        this.colors[barIndex%this.colors.length]
-                    );
 
-                    //draw bar value
-                    this.ctx.save();
-                    this.ctx.fillStyle = this.options.gridColor;
-                    this.ctx.font = "bold 11px Arial";
-                    this.ctx.fillText(val,
-                        this.options.padding + (barIndex * barSize + barSize/2 - barSize.toString().length*3),
-                        this.canvas.height - barHeight - this.options.padding - 5);
-                    this.ctx.restore();
+                    let val0 = this.options.data[categ];
 
-                    barIndex++;
+                    let firstOffset = 0;
+                    for (tagval in val0){
+
+                        if (!firstOffset){
+                            firstOffset++;
+
+                            // draw month title
+                            this.ctx.save();
+                            this.ctx.fillStyle = this.options.gridColor;
+                            this.ctx.font = "bold 11px Arial";
+                            this.ctx.fillText(categ,
+                                this.options.padding + (barIndex * barSize) + offset,
+                                this.canvas.height - this.options.padding + 11);
+                            this.ctx.restore();
+                        }
+
+                        let val = val0[tagval];
+                        //console.log(tagval + ': ' + val);
+
+                        let barHeight = Math.round( canvasActualHeight * val/maxValue);
+                        drawBar(
+                            this.ctx,
+                            this.options.padding + barIndex * barSize + offset,
+                            this.canvas.height - barHeight - this.options.padding,
+                            barSize,
+                            barHeight,
+                            this.colors[barIndex%this.colors.length]
+                        );
+
+                        //draw bar value
+                        this.ctx.save();
+                        this.ctx.fillStyle = this.options.gridColor;
+                        this.ctx.font = "bold 11px Arial";
+                        this.ctx.fillText(val,
+                            this.options.padding + (barIndex * barSize + barSize/2 - val.toString().length*2 ) + offset,
+                            this.canvas.height - barHeight - this.options.padding - 5);
+                        this.ctx.restore();
+                        barIndex++;
+                    }
+
+                    //offset += 10;
                 }
 
                 //drawing series name
-                this.ctx.save();
-                this.ctx.textBaseline="bottom";
-                this.ctx.textAlign="center";
-                this.ctx.fillStyle = "#000000";
-                this.ctx.font = "bold 14px Arial";
-                this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
-                this.ctx.restore();
+                // this.ctx.save();
+                // this.ctx.textBaseline="bottom";
+                // this.ctx.textAlign="center";
+                // this.ctx.fillStyle = "#000000";
+                // this.ctx.font = "bold 14px Arial";
+                // this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+                // this.ctx.restore();
 
                 //draw legend
                 barIndex = 0;
-                //let legend = document.querySelector("legend[for="+`'${this.canvas_id}'`+"]");
                 let legend = document.querySelector("legend[for='js_gistogramm_month']");
-                console.log(legend);
                 if (legend){
                     var ul = document.createElement("ul");
                     legend.append(ul);
-                    for (categ in this.options.data){
-                        var li = document.createElement("li");
-                        li.style.listStyle = "none";
-                        li.style.borderLeft = "20px solid "+this.colors[barIndex%this.colors.length];
-                        li.style.padding = "5px";
-                        li.textContent = categ;
-                        ul.append(li);
-                        barIndex++;
+                    for (categ0 in this.options.data){
+                        for (categ in this.options.data[categ0]){
+                            var li = document.createElement("li");
+                            li.style.listStyle = "none";
+                            li.style.borderLeft = "20px solid "+this.colors[barIndex%this.colors.length];
+                            li.style.padding = "5px";
+                            li.textContent = categ;
+                            ul.append(li);
+                            barIndex++;
+                        }
+                        break;
                     }
                 }
+
             }
         }
 
         var tagValues = {
-            "dohodi": 25000,
-            "rashodi": 15000,
-            "Internet": 2000,
-            "Svet/Elektro": 3000
+            'january' : {
+                "dohodi": 25000,
+                "rashodi": 15000,
+                "Internet": 2000,
+                "Svet/Elektro": 3000,
+            },
+            'february' : {
+                "dohodi": 33000,
+                "rashodi": 19000,
+                "Internet": 2100,
+                "Svet/Elektro": 2300,
+            },
+            'march' : {
+                "dohodi": 15000,
+                "rashodi": 29000,
+                "Internet": 2500,
+                "Svet/Elektro": 1100,
+            },
+            'aprile' : {
+                "dohodi": 17000,
+                "rashodi": 12000,
+                "Internet": 2100,
+                "Svet/Elektro": 1500,
+            },
         };
 
         var tagValuesChart = new Barchart(
